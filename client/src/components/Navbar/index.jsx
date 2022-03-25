ea9c9581776cd604599d960b21085f6ec85faba3
DESKTOP-NPTOF76\Gökalp AVCU <avcu219@gmail.com>
2022-03-25 10:11:55 +0300

    add navbar

--- client/src/components/Navbar/index.jsx
+++ client/src/components/Navbar/index.jsx
@@ -1,25 +1,27 @@
-import styles from "./styles.module.css"
-import {Link} from "react-router-dom";
+import styles from "./styles.module.css";
+import { Link } from "react-router-dom";
+import { Button, ButtonGroup } from "@chakra-ui/react";
 
 function Navbar() {
   return (
     <nav className={styles.nav}>
-   <div className={styles.left}>
-   <div className="logo">
-       <Link to="/">eCommerce</Link>
-   </div>
+      <div className={styles.left}>
+        <div className="logo">
+          <Link to="/">eCommerce</Link>
+        </div>
 
-   <ul className={styles.menu}>
-       <li>
-           <Link to="/">Products</Link>
-       </li>
-   </ul>
-   
-
-   </div>
-   <div className="right">right</div>
-  </nav>
-  )
+        <ul className={styles.menu}>
+          <li>
+            <Link to="/">Products</Link>
+          </li>
+        </ul>
+      </div>
+      <div className="right">
+        <Button colorScheme="pink">Register</Button>
+        <Button colorScheme='pink'>Button</Button>
+      </div>
+    </nav>
+  );
 }
 
-export default Navbar
+export default Navbar;
