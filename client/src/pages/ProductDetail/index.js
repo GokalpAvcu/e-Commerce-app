154b5064f474ce89f911b5c2190930e12b2d0407
DESKTOP-NPTOF76\Gökalp AVCU <avcu219@gmail.com>
2022-03-26 17:42:47 +0300

    add client

--- client/src/pages/ProductDetail/index.js
+++ client/src/pages/ProductDetail/index.js
@@ -1,11 +1,28 @@
-import {useParams} from "react-router-dom"
+import { useParams } from "react-router-dom";
 import { useQuery } from "react-query";
+import { fetchProduct } from "../../api";
+import { Box, Text, Button } from "@chakra-ui/react";
 
 function ProductDetail() {
-    const {product_id} = useParams ();
-  return (
-    <div>Product Detail {product_id}</div>
-  )
+  const { product_id } = useParams();
+
+  const { isLoading, isError, data } = useQuery(["product", product_id], () =>
+    fetchProduct(product_id)
+  );
+
+  if (isLoading) {
+    return <div>Loading...</div>;
+  }
+
+  if (isError) {
+    return <div>Error.</div>;
+  }
+
+  return <div>
+      <Button colorScheme="pink">
+          Add to basket
+      </Button>
+  </div>;
 }
 
-export default ProductDetail
+export default ProductDetail;
