9569161c569c7646b62afcafdfc1c36a3613cffa
DESKTOP-NPTOF76\Gökalp AVCU <avcu219@gmail.com>
2022-03-26 17:56:27 +0300

    add moment

--- client/src/pages/ProductDetail/index.js
+++ client/src/pages/ProductDetail/index.js
@@ -1,4 +1,5 @@
 import { useParams } from "react-router-dom";
+import {Moment} from 'react-moment';
 import { useQuery } from "react-query";
 import { fetchProduct } from "../../api";
 import { Box, Text, Button } from "@chakra-ui/react";
@@ -18,11 +19,18 @@ function ProductDetail() {
     return <div>Error.</div>;
   }
 
-  return <div>
-      <Button colorScheme="pink">
-          Add to basket
-      </Button>
-  </div>;
+  return (
+    <div>
+      <Button colorScheme="pink">Add to basket</Button>
+
+      <Text as="h2" fontSize="2xl">
+          {data.title}
+      </Text>
+      <Text>{Moment(data.createdAt).format("DD/MM/YYYY")}</Text>
+
+      <p>{data.description}</p>
+    </div>
+  );
 }
 
 export default ProductDetail;
