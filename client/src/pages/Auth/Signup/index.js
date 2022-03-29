180b54df5b61cb58f76189f174e83eb61e7b9e80
DESKTOP-NPTOF76\Gökalp AVCU <avcu219@gmail.com>
2022-03-29 16:37:48 +0300

    add formik, yup

--- client/src/pages/Auth/Signup/index.js
+++ client/src/pages/Auth/Signup/index.js
@@ -1,14 +1,48 @@
-import React from 'react'
-import {Flex, Box, Heading, FormControl, FormLabel} from "@chakra-ui/react";
+import React from "react";
+import {
+  Flex,
+  Box,
+  Heading,
+  FormControl,
+  FormLabel,
+  Input,
+  Button
+} from "@chakra-ui/react";
 
 function Signup() {
   return (
     <div>
       <Flex align="center" width="full" justifyContent="center">
-      
+        <Box pt={10}>
+          <Box textAlign="center">
+            <Heading>Sign Up</Heading>
+          </Box>
+          <Box my={5} textAlign="left">
+            <form onSubmit={() => {}}>
+              <FormControl>
+                <FormLabel>E-mail</FormLabel>
+                <Input name="email" />
+              </FormControl>
+
+              <FormControl mt="4">
+                <FormLabel>Password</FormLabel>
+                <Input name="password" type="password" />
+              </FormControl>
+
+              <FormControl>
+                <FormLabel>Password Confirm</FormLabel>
+                <Input name="password confirm" type="password" />
+              </FormControl>
+
+              <Button mt="4" width="full" type="submit">
+               Sign Up
+              </Button>
+            </form>
+          </Box>
+        </Box>
       </Flex>
     </div>
-  )
+  );
 }
 
-export default Signup
+export default Signup;
