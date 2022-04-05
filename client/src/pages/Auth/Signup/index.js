8a322114827583cf6814bcaaad6e8455994ca036
DESKTOP-NPTOF76\Gökalp AVCU <avcu219@gmail.com>
2022-04-05 16:35:10 +0300

    add validation

--- client/src/pages/Auth/Signup/index.js
+++ client/src/pages/Auth/Signup/index.js
@@ -9,20 +9,22 @@ import {
   Button,
 } from "@chakra-ui/react";
 
-import {useFormik} from "formik";
+import { useFormik } from "formik";
+import validationSchema from "./validations"; 
 
 function Signup() {
   const formik = useFormik({
     initialValues: {
-      email:"",
-      password:"",
+      email: "",
+      password: "",
       passwordConfirm: "",
     },
-    onSubmit: async (values,bag) => {
+    validationSchema,
+    onSubmit: async (values, bag) => {
       console.log(values);
     },
   });
-  
+
   return (
     <div>
       <Flex align="center" width="full" justifyContent="center">
@@ -31,20 +33,37 @@ function Signup() {
             <Heading>Sign Up</Heading>
           </Box>
           <Box my={5} textAlign="left">
-            <form onSubmit={() => {}}>
+            <form onSubmit={formik.handleSubmit}>
               <FormControl>
                 <FormLabel>E-mail</FormLabel>
-                <Input name="email" />
+                <Input
+                  name="email"
+                  onChange={formik.handleChange}
+                  onBlur={formik.handleBlur}
+                  value={formik.values.email}
+                />
               </FormControl>
 
-               <FormControl mt="4">
+              <FormControl mt="4">
                 <FormLabel>Password</FormLabel>
-                <Input name="password" type="password" />
+                <Input
+                  name="password"
+                  type="password"
+                  onChange={formik.handleChange}
+                  onBlur={formik.handleBlur}
+                  value={formik.values.password}
+                />
               </FormControl>
 
               <FormControl>
                 <FormLabel>Password Confirm</FormLabel>
-                <Input name="password confirm" type="password" />
+                <Input
+                  name="password confirm"
+                  type="password"
+                  onChange={formik.handleChange}
+                  onBlur={formik.handleBlur}
+                  value={formik.values.passwordConfirm}
+                />
               </FormControl>
 
               <Button mt="4" width="full" type="submit">
