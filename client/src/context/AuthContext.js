438cb07f4f877f385806b3dfe1a0fca9141975f0
DESKTOP-NPTOF76\Gökalp AVCU <avcu219@gmail.com>
2022-04-06 14:34:09 +0300

    add client

--- client/src/context/AuthContext.js
+++ client/src/context/AuthContext.js
@@ -4,4 +4,20 @@ const AuthContext = createContext();
 
 const AuthProvider = ({children}) => {
    const [user,setUser] = useState(null); // user kayıt olduğunda detay bilgilerini tutar
+   const [loggedIn, setLoggedIn] = useState(false)
+
+   const login = (data) => {
+       setLoggedIn(true);
+       setUser(data);
+   };
+
+   const values = {
+       loggedIn,
+       user,
+       login,
+   };
+   return <AuthContext.Provider value={values}>{children}</AuthContext.Provider>
 }
+const useAuth = () => useContext(AuthContext);
+
+export{ AuthProvider, useAuth };
